d31dfabd4b49494f874cbbe894ea83ef3bad7c49
Giorgos Neokleous <neokleoys2005@gmail.com>
2020-03-22 13:35:20 +0000

    Add default value to commands and error message if command 1 not found

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,23 @@
 #!/bin/sh -l
 set -e
 
-COMMAND_1=${1}
-COMMAND_2=${2}
-COMMAND_3=${3}
-COMMAND_4=${4}
-COMMAND_5=${5}
+COMMAND_1="${1}"
+COMMAND_2="${2}"
+COMMAND_3="${3}"
+COMMAND_4="${4}"
+COMMAND_5="${5}"
 
 echo "Command 1: $COMMAND_1"
 echo "Command 2: $COMMAND_2"
 echo "Command 3: $COMMAND_3"
 echo "Command 4: $COMMAND_4"
 echo "Command 5: $COMMAND_5"
+
+if [ -z $COMMAND_1 ]
+then
+    echo "Please provide at least the first command"
+    exit 1
+fi;
 
 eval $COMMAND_1
 eval $COMMAND_2
