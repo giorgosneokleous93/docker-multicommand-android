56fe4b7533c9ffeb458787cfb3dbcfae2b74f82c
Giorgos Neokleous <neokleoys2005@gmail.com>
2020-03-22 16:49:31 +0000

    comment out everything in favour of replacing with Github Example to check if arguments are working for the example

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,28 @@
 #!/bin/sh -l
 
-COMMAND_1="${1}"
-COMMAND_2="${2}"
-COMMAND_3="${3}"
-COMMAND_4="${4}"
-COMMAND_5="${5}"
+# COMMAND_1="${1}"
+# COMMAND_2="${2}"
+# COMMAND_3="${3}"
+# COMMAND_4="${4}"
+# COMMAND_5="${5}"
 
-echo "Command 1: $COMMAND_1"
-echo "Command 2: $COMMAND_2"
-echo "Command 3: $COMMAND_3"
-echo "Command 4: $COMMAND_4"
-echo "Command 5: $COMMAND_5"
+# echo "Command 1: $COMMAND_1"
+# echo "Command 2: $COMMAND_2"
+# echo "Command 3: $COMMAND_3"
+# echo "Command 4: $COMMAND_4"
+# echo "Command 5: $COMMAND_5"
 
-if [ -z $COMMAND_1 ]; then
-    echo "Please provide at least the first command"
-    exit 1
-fi
+# if [ -z $COMMAND_1 ]; then
+#     echo "Please provide at least the first command"
+#     exit 1
+# fi
 
-eval $COMMAND_1
-eval $COMMAND_2
-eval $COMMAND_3
-eval $COMMAND_4
-eval $COMMAND_5
+# eval $COMMAND_1
+# eval $COMMAND_2
+# eval $COMMAND_3
+# eval $COMMAND_4
+# eval $COMMAND_5
+
+echo "Hello $1"
+time=$(date)
+echo "::set-output name=time::$time"
