903f4aaf841b70b3ad29b189cf06a376664e861d
Giorgos Neokleous <neokleoys2005@gmail.com>
2020-03-22 11:03:17 +0000

    add basic config file for the action

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,14 @@ COMMAND_3=${3}
 COMMAND_4=${4}
 COMMAND_5=${5}
 
-eval COMMAND_1
-eval COMMAND_2
-eval COMMAND_3
-eval COMMAND_4
-eval COMMAND_5
+echo "Command 1: $COMMAND_1"
+echo "Command 2: $COMMAND_2"
+echo "Command 3: $COMMAND_3"
+echo "Command 4: $COMMAND_4"
+echo "Command 5: $COMMAND_5"
+
+eval $COMMAND_1
+eval $COMMAND_2
+eval $COMMAND_3
+eval $COMMAND_4
+eval $COMMAND_5
